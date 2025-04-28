889f19dd3a2e7885af86e04e459ab74b25665c46
TheAihopGG <theaihopgg@gmail.com>
2025-04-28 20:28:48 +0700

    fix

--- scenes/global_vars/global_vars.gd
+++ scenes/global_vars/global_vars.gd
@@ -3,13 +3,19 @@ extends Node
 signal game_over()
 signal paused()
 signal resume()
+signal mute_toggled()
 
 var score: int = 0
 var obstacle_spawner: ObstacleSpawner
 var world: World
 var player: Player
-var volume: int = 50
+var music_volume: int = 50
+var sfx_volume: int
 var difficulty: int = 3
+var is_muted: bool = false:
+	set(value):
+		is_muted = value
+		mute_toggled.emit(value)
 var process_enabled: bool:
 	get:
 		return not is_paused and not is_game_over
@@ -30,3 +36,7 @@ class Groups:
 	static var BULLET = "bullet"
 	static var OBSTACLE = "obstacle"
 	static var GAME_OVER_AREA = "game_over_area"
+
+func _process(_delta):
+	if Input.is_action_just_pressed("mute"):
+		is_muted = not is_muted
--- scenes/settings_menu/settings_menu.gd
+++ scenes/settings_menu/settings_menu.gd
@@ -1,21 +1,28 @@
 extends Control
 
-@onready var volume_value_label: Label = %VolumeValueLabel
-@onready var texture_rect: TextureRect = %TextureRect
+@onready var sfx_volume_value_label: Label = %SFXVolumeValueLabel
+@onready var sfx_volume_scroll_bar: HScrollBar = %SFXVolumeScrollBar
+@onready var music_volume_value_label: Label = %MusicVolumeValueLabel
+@onready var music_volume_scroll_bar: HScrollBar = %MusicVolumeScrollBar
 @onready var difficulty_value_label: Label = %DifficultyValueLabel
-@onready var volume_scroll_bar: HScrollBar = %VolumeScrollBar
 @onready var difficulty_scroll_bar: HScrollBar = %DifficultyScrollBar
+@onready var mute_check_button: CheckButton = %MuteCheckButton
 
 func _ready() -> void:
+	GlobalVars.mute_toggled.connect(func (value): mute_check_button.button_pressed = value)
 	difficulty_scroll_bar.set_value_no_signal(GlobalVars.difficulty)
-	volume_scroll_bar.set_value_no_signal(GlobalVars.volume)
-	volume_value_label.text = str(GlobalVars.volume)
+	music_volume_scroll_bar.set_value_no_signal(GlobalVars.music_volume)
+	sfx_volume_scroll_bar.set_value_no_signal(GlobalVars.sfx_volume)
+	
 	difficulty_value_label.text = str(GlobalVars.difficulty)
+	music_volume_value_label.text = str(GlobalVars.music_volume)
+	sfx_volume_value_label.text = str(GlobalVars.sfx_volume)
+	
+	mute_check_button.button_pressed = GlobalVars.is_muted
 
-
-func _on_volume_scroll_bar_value_changed(value: float) -> void:
-	GlobalVars.volume = int(value)
-	volume_value_label.text = str(GlobalVars.volume)
+func _on_music_volume_scroll_bar_value_changed(value: float) -> void:
+	GlobalVars.music_volume = int(value)
+	sfx_volume_scroll_bar.text = str(GlobalVars.music_volume)
 
 
 func _on_difficulty_scroll_bar_value_changed(value: float) -> void:
@@ -23,5 +30,14 @@ func _on_difficulty_scroll_bar_value_changed(value: float) -> void:
 	difficulty_value_label.text = str(GlobalVars.difficulty)
 
 
+func _on_sfx_volume_scroll_bar_value_changed(value: float) -> void:
+	GlobalVars.sfx_volume = int(value)
+	sfx_volume_value_label.text = str(GlobalVars.sfx_volume)
+
+
 func _on_back_to_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+
+
+func _on_mute_check_button_toggled(toggled_on: bool) -> void:
+	GlobalVars.is_muted = toggled_on
