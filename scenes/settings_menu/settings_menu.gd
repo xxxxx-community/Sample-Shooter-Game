b87cea44d2768469b2cc33b9a3e4f243bc311d11
TheAihopGG <theaihopgg@gmail.com>
2025-04-28 20:00:57 +0700

    fix

--- scenes/settings_menu/settings_menu.gd
+++ scenes/settings_menu/settings_menu.gd
@@ -7,28 +7,20 @@ extends Control
 @onready var difficulty_scroll_bar: HScrollBar = %DifficultyScrollBar
 
 func _ready() -> void:
-	difficulty_scroll_bar.value = GlobalVars.volume
-	volume_scroll_bar.value = GlobalVars.difficulty
-	volume_value_label.text = "%" + str(GlobalVars.volume)
+	difficulty_scroll_bar.set_value_no_signal(GlobalVars.difficulty)
+	volume_scroll_bar.set_value_no_signal(GlobalVars.volume)
+	volume_value_label.text = str(GlobalVars.volume)
 	difficulty_value_label.text = str(GlobalVars.difficulty)
 
-
 func _on_volume_scroll_bar_value_changed(value: float) -> void:
-	volume_value_label.text = "%" + str(int(value))
 	GlobalVars.volume = int(value)
+	volume_value_label.text = str(GlobalVars.volume)
 	
 
 func _on_difficulty_scroll_bar_value_changed(value: float) -> void:
-	match int(value):
-		5:
-			texture_rect.visible = true
-			difficulty_value_label.visible = false
-		var difficulty:
-			texture_rect.visible = false
-			difficulty_value_label.visible = true
-			difficulty_value_label.text = str(difficulty)
 	GlobalVars.difficulty = int(value)
-	
+	difficulty_value_label.text = str(GlobalVars.difficulty)
+
 
 func _on_back_to_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
